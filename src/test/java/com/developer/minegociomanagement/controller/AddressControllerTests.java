a79663a13cdca54f0afa4f45bb9b985176b173c0
Andres Ruiz <andyruizz_db@hotmail.com>
2023-02-26 00:38:42 -0500

    add controllers and services unit test

---
+++ src/test/java/com/developer/minegociomanagement/controller/AddressControllerTests.java
@@ -0,0 +1,13 @@
+package com.developer.minegociomanagement.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressControllerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/developer/minegociomanagement/service/AddressServiceImplTests.java
@@ -0,0 +1,13 @@
+package com.developer.minegociomanagement.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressServiceImplTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
